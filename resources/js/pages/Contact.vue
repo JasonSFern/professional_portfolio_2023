b3eaec54385cf7d9d2fe6d4830678cb189e63e1d
jsfern774 <35469697+jsfern774@users.noreply.github.com>
2022-02-09 11:51:15 -0700

    Updated: Contact page functionality

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -8,32 +8,50 @@
             <v-col class="col-md-2"></v-col>
             <v-col class="col-md-8">
               <div class="email-box email-margin">
-                <div class="email-head-padding">
-                  <v-icon color="#F93D3D" small>mdi-checkbox-blank-circle</v-icon>
-                  <v-icon color="#FCC42F" small>mdi-checkbox-blank-circle</v-icon>
-                  <v-icon color="#2AC823" small>mdi-checkbox-blank-circle</v-icon>
+                <div class="d-flex">
+                  <div class="col-md-4">
+                    <v-icon color="#F93D3D" small>mdi-checkbox-blank-circle</v-icon>
+                    <v-icon color="#FCC42F" small>mdi-checkbox-blank-circle</v-icon>
+                    <v-icon color="#2AC823" small>mdi-checkbox-blank-circle</v-icon>
+                  </div>
+                  <div class="col-md-4" style="text-align:center">Get in touch</div>
+                  <div class="col-md-4"></div>
                 </div>
                 <div class="email-box-padding">
+                  <div class="d-flex pa-0">
+                    <div class="d-flex col-md-3 pa-0">
+                      <v-icon :color="this.icon.name.color" class="pr-2 pa-0">{{ this.icon.name.icon }}</v-icon><p class="blue-text"><strong>Name:&nbsp;</strong></p>
+                    </div>
+                    <div class="col-md-9">
+                      <input class="contact-input" v-model="name"/>
+                    </div>
+                  </div>
+                  <div v-if="showBy.name" class="d-flex pa-0">
+                    <div class="d-flex col-md-3 pa-0">
+                      <v-icon :color="this.icon.email.color" class="pr-2 pa-0">{{ this.icon.email.icon }}</v-icon><p class="blue-text"><strong>Email:&nbsp;</strong></p>
+                    </div>
+                    <div class="col-md-9">
+                      <input class="contact-input" v-model="email"/>
+                    </div>
+                  </div>
+                  <div v-if="showBy.name != '' && showBy.email != ''" class="d-flex pa-0">
+                    <div class="d-flex col-md-3 pa-0">
+                      <v-icon :color="this.icon.subject.color" class="pr-2 pa-0">{{ this.icon.subject.icon }}</v-icon><p class="blue-text"><strong>Subject:&nbsp;</strong></p>
+                    </div>
+                    <div class="col-md-9">
+                      <input class="contact-input" v-model="subject"/>
+                    </div>
+                  </div>
+                  <div v-if="showBy.name != '' && showBy.email != '' && showBy.subject != ''" class="d-flex pa-0">
+                    <div class="d-flex col-md-3 pa-0">
+                      <v-icon :color="this.icon.message.color" class="pr-2 pa-0 message-icon">{{ this.icon.message.icon }}</v-icon><p class="blue-text"><strong>Message:&nbsp;</strong></p>
+                    </div>
+                    <div class="col-md-9">
+                      <textarea rows="5" class="contact-input" style="resize: none !important;" v-model="message"/>
+                    </div>
+                  </div>
 
-                  <p>Please fill out</p><br>
-                  <div class="py-4">
-                    <p class="blue-text"><strong>Name</strong></p><br>
-                    <v-icon :color="this.nameStyle.color" class="pr-2">{{ this.nameStyle.icon }}</v-icon><input :value="this.name"/><br/>
-                  </div>
-                  <div class="py-4">
-                    <p class="blue-text"><strong>Email</strong></p><br>
-                    <v-icon :color="this.emailStyle.color" class="pr-2">{{ this.emailStyle.icon }}</v-icon><input :value="this.email"/><br/>
-                  </div>
-                  <div class="py-4">
-                    <p class="blue-text"><strong>Subject</strong></p><br>
-                    <v-icon :color="this.subjectStyle.color" class="pr-2">{{ this.subjectStyle.icon }}</v-icon><input :value="this.subject"/><br/>
-                  </div>
-                  <div class="py-4">
-                    <p class="blue-text"><strong>Content</strong></p><br>
-                    <v-icon :color="this.messageStyle.color" class="pr-2">{{ this.messageStyle.icon }}</v-icon><input rows="5" :value="this.message"/><br/>
-                  </div>
-
-                  <div class="d-flex flex-wrap-reverse flex-column-reverse">
+                  <div v-if="showBy.name != '' && showBy.email != '' && showBy.subject != '' && showBy.message != ''" class="d-flex flex-wrap-reverse flex-column-reverse">
                     <button class="col-2 email-button">Send</button>
                   </div>
                 </div>
@@ -86,13 +104,55 @@ export default {
       }
     },
   },
+  watch: {
+    name: function(value) {
+      this.validateRegular(value, 'name')
+    },
+    email: function(value) {
+      this.validateEmail(value, 'email')
+    },
+    subject: function(value) {
+      this.validateRegular(value, 'subject')
+    },
+    message: function(value) {
+      if (value != '') {
+        this.setIconSuccess('message')
+      } else {
+        this.setIconFocus('message')
+      }
+    },
+  },
   data() {
     return {
-      name: 'Jason Fernandes',
-      email: 'jsfernandes83@gmail.com',
-      subject: 'Test email',
-      message: 'This is a test email with content typed out',
+      name: 'a',
+      email: 'j@g.com',
+      subject: 'a',
+      message: 'a',
       status: 'STATUS TEST',
+      icon: {
+        name: {
+          icon: 'mdi-arrow-right-bold-circle',
+          color: '#ea4aaa',
+        },
+        email: {
+          icon: 'mdi-arrow-right-bold-circle',
+          color: '#ea4aaa',
+        },
+        subject: {
+          icon: 'mdi-arrow-right-bold-circle',
+          color: '#ea4aaa',
+        },
+        message: {
+          icon: 'mdi-arrow-right-bold-circle',
+          color: '#ea4aaa',
+        },
+      },
+      showBy: {
+        name: false,
+        email: false,
+        subject: false,
+        message: false,
+      },
       displayVariables: {
         icons: {
           focus: 'mdi-arrow-right-bold-circle',
@@ -108,12 +168,42 @@ export default {
     }
   },
   methods: {
-    validateInput(value, type) {
-      console.log('HEy')
-      if (value == 'Jason') {
-        return true
+    setIconFocus(key) {
+      this.showBy[key] = false
+      this.icon[key].icon = this.displayVariables.icons.focus
+      this.icon[key].color = this.displayVariables.colors.focus
+    },
+    setIconSuccess(key) {
+      this.showBy[key] = true
+      this.icon[key].icon = this.displayVariables.icons.success
+      this.icon[key].color = this.displayVariables.colors.success
+    },
+    setIconDanger(key) {
+      this.showBy[key] = false
+      this.icon[key].icon = this.displayVariables.icons.danger
+      this.icon[key].color = this.displayVariables.colors.danger
+    },
+    validateRegular(value, inputName) {
+      if (value != '') {
+        if(!/[^a-zA-Z0-9 ]/.test(value)) {
+          this.setIconSuccess(inputName)
+        } else {
+          this.setIconDanger(inputName)
+        }
       } else {
-        return false
+        this.setIconFocus(inputName)
+      }
+    },
+    validateEmail(value, inputName) {
+      if (value != '') {
+        var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(filter.test(value)) {
+          this.setIconSuccess(inputName)
+        } else {
+          this.setIconDanger(inputName)
+        }
+      } else {
+        this.setIconFocus(inputName)
       }
     }
   }
@@ -141,10 +231,14 @@ export default {
   padding: 15px !important;
 }
 
-.email-button :disabled {
-  color: #627597;
+.email-button {
+  color: white;
   background: none;
-  border-color: #627597;
+  background-color: #2e333f;
+  border: 1px solid #868f9e;
+  border-radius: 5px !important;
+  padding: 5px;
+  margin-right:12px;
 }
 
 .email-status-test {
@@ -158,6 +252,7 @@ export default {
 .blue-text {
   color: #00cfc8;
   margin-bottom: 0px;
+  margin-top:18px;
 }
 
 .green-text {
@@ -189,5 +284,30 @@ textarea, input {
   position: absolute;
   width: 100%;
 }
+
+.message-icon {
+  align-items: normal !important;
+  margin-top:18px !important;
+}
+
+.contact-input {
+  background-color: #122247;
+  border: 1px solid #252c3f;
+  border-radius: 5px !important;
+  padding: 5px;
+  width:100%;
+}
+/* // */
+
+.enter { transform: translateY(100%) }
+.enter-to { transform: translateY(0) }
+.slide-enter-active { position: absolute }
+
+.leave { transform: translateY(0) }
+.leave-to { transform: translateY(-100%) }
+
+.slide-enter-active,
+.slide-leave-active { transition: all 750ms ease-in-out }
+
 
 </style>
