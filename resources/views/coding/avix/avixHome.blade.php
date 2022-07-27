2083ed7072d9137bf8d84fceafa2e06f60338f7b
Jason <jsfernandes83@gmail.com>
2022-07-27 16:04:50 -0600

    Implemented updated Avix website project

--- resources/views/coding/avix/avixHome.blade.php
+++ resources/views/coding/avix/avixHome.blade.php
@@ -1,295 +1,246 @@
-@extends('coding.avix.avixLayout')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"
+    ></script>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossorigin="anonymous"
+    />
+        <link rel="stylesheet" href="/css/avix.css">
 
-@section('content')
+    <title>AVIX AIR</title>
+  </head>
 
-  <title>Welcome to AVIX</title>
+  <!-- HEADER / NAVIGATION -->
+  <header id="sectionHeader">
+    <div class="fade-in-nav logo floatleft"></div>
 
-</head>
+    <ul class="fade-in-nav desktop-nav">
+      <li class="goAbout">ABOUT US</li>
+      <li class="goProducts">CURRENT PRODUCTS</li>
+      <li class="goUpcoming">UPCOMING PRODUCTS</li>
+    </ul>
 
-
-<!-- HEADER, DESKTOP NAVIGATION -->
-<header>
-
-<div class="wrapper desktop-nav">
-
-  <a href="/avix/" class="logo floatleft"></a>
-
-  <ul>
-      <li><a href="/avix/about">ABOUT AVIX</a></li>
-      <li><a href="/avix/products">PRODUCTS</a></li>
-      <li><a href="/avix/contact">CONTACT US</a></li>
-  </ul>
-
-</div>
-
-
-<!-- HEADER, MOBILE NAVIGATION -->
-<div class="wrapper mobile-nav">
-
-  <a href="/avix/about" class="logo floatleft"></a>
-
-  <nav role='navigation'>
+    <div class="fade-in-nav mobile-nav">
       <div class="menu-toggle">
-
-      <input type="checkbox">
+        <input type="checkbox">
           <span></span>
           <span></span>
           <span></span>
-      </input>
-
-      <ul class="menu">
-          <a href="/avix/about"><li>About AVIX</li></a>
-          <a href="/avix/products"><li>Products</li></a>
-          <a href="/avix/contact"><li>Contact Us</li></a>
-      </ul>
-
-      </div>
-  </nav>
-</div>
-
-</header>
-
-
-<!-- HERO, DESKTOP NAVIGATION -->
-<div class= "hero-overlay desktop-nav">
-  <div class="hero">
-
-      <div class="hero-text-container">
-          <h1>INTRODUCING</h1>
-          <h1>FLIGHT...</h1>
-      </div>
-
-      <section class="hero-text-container-2">
-          <h2 class="floatleft">REDEFINED</h2>
-      </section>
-
-  </div>
-</div>
-
-
-<!-- HERO, MOBILE NAVIGATION -->
-<div class="mobile-nav">
-  <div class="hero">
-
-      <div class="hero-text-container">
-          <h1>INTRODUCING</h1>
-          <h1>FLIGHT...</h1>
-      </div>
-
-      <div class="hero-text-container-2">
-          <h2 class="alignleft">REDEFINED</h2>
-      </div>
-
-  </div>
-</div>
-
-
-<!-- QUOTE, DESKTOP NAVIGATION -->
-<div class= "desktop-nav cloud-overlay quote-container">
-
-<div class="quote-text-container">
-    <h3>"Since the dawn of man, it has been a dream to fly. But now that we have achieved that, it is time to expand our horizons, and innovate where no one else has before."</h3>
-</div>
-
-<div class="quote-text-container">
-
-    <div>
-      <h4 class="alignright">Jason Fernandes</h4>
-      <h4 class="alignright">Founder and CEO</h4>
-    </div>
-
-</div>
-</div>
-
-
-<!-- QUOTE, MOBILE NAVIGATION -->
-<div class="mobile-nav">
-<div class="quote-container">
-
-    <div class="quote-text-container">
-        <h3>"Since the dawn of man, it has been a dream to fly. But now that we have achieved that, it is time to expand our horizons, and innovate where no one else has before."</h3>
-    </div>
-
-    <div class="spacer">
-        <div class="quote-text-container">
-            <h4 class="alignright">Jason Fernandes</h4>
-            <h4 class="alignright">Founder and CEO</h4>
-        </div>
-    </div>
-
-</div>
-</div>
-
-
-<!-- FLAGSHIP, DESKTOP NAVIGATION -->
-<div class="flagship-overlay desktop-nav ">
-<div class="flagship-container">
-
-    <div class="flagship-text-container">
-        <h2 class="alignright">INDULGE IN LUXURY</h2>
-    </div>
-
-    <div class="flagship-text-container">
-        <h4>in PEACE...</h4>
-        <br>
-        <h4>in COMFORT...</h4>
-        <br>
-        <h4>in SERENITY...</h4>
-        <br>
-    </div>
-
-</div>
-</div>
-
-
-<!-- FLAGSHIP, MOBILE NAVIGATION -->
-<div class="mobile-nav">
-<div class="flagship-container">
-
-    <div class="flagship-text-container">
-        <h2 class="alignright">INDULGE IN LUXURY</h2>
-    </div>
-
-    <div class="flagship-text-container">
-        <h4>in PEACE...</h4>
-        <br>
-        <h4>in COMFORT...</h4>
-        <br>
-        <h4>in SERENITY...</h4>
-    </div>
-
-</div>
-</div>
-
-
-<!-- FLAGSHIP FROSTED GLASS, DESKTOP NAVIGATION -->
-<section class="desktop-nav frosted-effect">
-<div class="aligncenter frosted-glass">
-
-    <h2>Our Airships</h2>
-    <div class="frosted-glass-text">
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-    </div>
-    <br>
-    <br>
-    <a href="/avix/products" class="button">EXPLORE</a>
-
-</div>
-</section>
-
-
-<!-- FLAGSHIP GRADIENT, MOBILE NAVIGATION -->
-
-<section class="mobile-nav">
-
-<div class="aligncenter gradient">
-
-    <h2>Our Airships</h2>
-
-    <div class="frosted-glass-text">
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-    </div>
-    <br>
-    <br>
-    <a href="/avix/products" class= "button">EXPLORE</a>
-
-</div>
-</section>
-
-
-<!-- CONTACT, DESKTOP AND MOBILE NAVIGATION -->
-<div class="contact-c2a">
-<div class="contact-c2a-container">
-
-    <div class="aligncenter contact-c2a-text-container">
-        <h1>Contact Us</h1>
-        <br>
-        <br>
-        <br>
-        <a href="/avix/contact" class= "button button-big"><i class="fa fa-envelope fa-5x" aria-hidden="true"></i></a>
-    </div>
-</div>
-</div>
-
-</body>
-
-
-<!-- FOOTER, DESKTOP NAVIGATION -->
-<footer>
-<div class="desktop-nav row">
-
-<div class="col-12 col-md-2">
-    <a href="/avix/" class="logo floatleft"></a>
-</div>
-
-<div class="col-12 col-md-9">
-    <div class=row>
-
-        <div class="col-12 col-md-3">
-            <li><a href="/avix/about"><h1>About Avix</h1></a></li>
-        </div>
-
-        <div class="col-12 col-md-3">
-            <li><a href="/avix/products"><h1>Products</h1></a></li>
-        </div>
-
-        <div class="col-12 col-md-3">
-            <li><a href="/avix/contact"><h1>Contact Us</h1></a></li>
-        </div>
-
-        <div class="col-12 col-md-3">
-            <ul class='social-icons'>
-                <li><a href='#'><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></a></li>
-                <li><a href='#'><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a></li>
-                <li><a href='#'><i class="fa fa-youtube fa-3x" aria-hidden="true"></i></a></li>
-                <li><a href='#'><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-
-    </div>
-</div>
-</div>
-
-
-<!-- FOOTER, MOBILE NAVIGATION -->
-<div class=mobile-nav>
-
-<section>
-    <a href="/avix/" class="logo floatleft"></a>
-</section>
-
-<br>
-<br>
-<br>
-<br>
-
-<div>
-
-    <div class="button-spacer">
-        <a href="/avix/about" class= "button-footer">ABOUT AVIX</a>
-    </div>
-
-    <div class="button-spacer">
-        <a href="/avix/products" class= "button-footer">PRODUCTS</a>
-    </div>
-
-    <div class="button-spacer">
-        <a href="/avix/contact" class= "button-footer">CONTACT US</a>
-    </div>
-
-    <div class="button-spacer">
-        <ul class='social-icons-footer'>
-            <li><a href='#'><i class="fa fa-facebook-official fa-5x" aria-hidden="true"></i></a></li>
-            <li><a href='#'><i class="fa fa-twitter fa-5x" aria-hidden="true"></i></a></li>
-            <li><a href='#'><i class="fa fa-youtube fa-5x" aria-hidden="true"></i></a></li>
-            <li><a href='#'><i class="fa fa-instagram fa-5x" aria-hidden="true"></i></a></li>
+        </input>
+      
+        <ul class="menu alignleft">
+          <li class="goAbout">ABOUT US</li>
+          <li class="goProducts">CURRENT PRODUCTS</li>
+          <li class="goUpcoming">UPCOMING PRODUCTS</li>
         </ul>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <!-- HERO -->
+    <div id="sectionHero">
+      <div class="hero-overlay">
+        <div class="hero-container d-flex flex-column justify-content-between">
+          <div class="statement-container">
+            <h1 class="fade-in-hero-1">INTRODUCING</h1>
+            <h1 class="fade-in-hero-1">FLIGHT...</h1>
+          </div>
+          <div class="statement-container height-auto"></div>
+          <div class="d-flex flex-row-reverse statement-container">
+            <div>
+              <h2 class="fade-in-hero-2">REDEFINED</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
-</div>
+    <!-- ABOUT -->
+    <div id="sectionAbout" class="cloud-overlay">
+      <div class="quote-container aligncenter">
+        <h3>
+          In 2008 we started in a small airplane hangar on the south side of San
+          Fransico. Today we have become to leading innovator in luxury air
+          travel. From our airships to our private jets, we make sure that not
+          only do you travel in style, but luxury as well
+        </h3>
+      </div>
 
-</div>
-</footer>
+      <div class="statement-container aligncenter">
+        <h2 class="">MEET THE TEAM</h2>
+      </div>
 
+      <div class="d-md-flex justify-content-start">
+        <div class="item-card col-12 col-md-4">
+          <h4>Jason Fernandes</h4>
+          <img class="image-circle" src="../img/avix/sil1.jpg" />
+          <h5>Founder and CEO</h5>
+          <p>
+            As the CEO of Avix, Jason Fernandes is responsible for running all facets of the business. Jason has a proven executive management track record and over 20 years of experience driving sales growth in the aviation industry.
+          </p>
+        </div>
 
-@endsection
+        <div class="item-card col-12 col-md-4">
+          <h4>Erika Langdon</h4>
+          <img class="image-circle" src="../img/avix/sil2.jpg" />
+          <h5>CFO</h5>
+          <p>
+            As CFO of Avix, Erika Langdon is responsible for operational and financial strategy, assessment of organic and acquisition expansion opportunities, governance and risk management. Erika has been instrumental in the company's growth initiatives.
+          </p>
+        </div>
+
+        <div class="item-card col-12 col-md-4">
+          <h4>Sam Amante</h4>
+          <img class="image-circle" src="../img/avix/sil3.jpg" />
+          <h5>Lead Engineer</h5>
+          <p>
+            As Lead Engineer of Avix, Sam Amante is responsible for improving our product designs while reducing costs and streamlining manufacturing processes. Sam's 10+ years of experience as a Aeronautical Engineer has proven instrumental to improving the company's output.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- PRODUCTS -->
+    <div id="sectionProducts">
+      <div class="products-overlay">
+        <div class="products-container">
+          <div class="statement-container">
+            <h2 class="aligncenter">OUR PRODUCTS</h2>
+            <br />
+            <h3 class="aligncenter">
+              Three distinct choices to compliment your lifestyle
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <section class="frosted-effect">
+        <div class="frosted-glass">
+          <div class="d-md-flex justify-content-start">
+            <div class="item-card col-12 col-md-4">
+              <img class="image-circle" src="../img/avix/ship.jpg" />
+              <h5>Open Space</h5>
+              <p>
+                The Open Space is a mega-yacht in the sky. This king-size airship is beautifully designed and customizable. Open Space, as the name implies, is very spacious throughout with 10 large staterooms, an open concept salon, professionally set-up pilot house, office, laundry room, and a walk around engine room. The latest high-end finishes and features and the latest in modern performance technology is included standard.
+              </p>
+            </div>
+            <div class="item-card col-12 col-md-4">
+              <img class="image-circle" src="../img/avix/jet.jpg" />
+              <h5>Private Space</h5>
+              <p>
+                The Private Space is a private jet that is incredibly luxurious with full stand-up cabins and loads of space for up to 18 passengers and their luggage. Standard amenities included are large comfortable chairs, lie-flat beds, enclosed bathrooms, top tier entertainment facilities, and onboard Wi-Fi. The Private Space can travel distances up to 5,700 miles at a cruise speed of about 550mph.
+              </p>
+            </div>
+            <div class="item-card col-12 col-md-4">
+              <img class="image-circle" src="../img/avix/personalp.jpg" />
+              <h5>Personal Space</h5>
+              <p>
+                The Personal Space is a vertical take-off and landing jet and is our first fully electric offering. This eVTOL features electric jet engines that are integrated into the wing flaps. This provides increased payload along with reduced noise. A fixed wing design allows for greater aerodynamics and makes the Personal Space very efficient at cruise speeds. Available cabins offer a 1 or 2 person capacity with luxurious seating for the occupant(s).
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <!-- UPCOMING -->
+    <div id="sectionUpcoming" class="halo-overlay">
+      <div class="halo-container d-flex flex-column justify-content-between">
+        <div class="d-flex flex-row-reverse statement-container">
+          <div>
+            <h1 class="alignright">PRESENTING:</h1>
+            <h1 class="alignright">HALO VR-6</h1>
+            <h2 style="font-size: 28px !important" class="alignright">
+              The most advanced ship in it's class
+            </h2>
+          </div>
+        </div>
+        <div class="statement-container height-auto"></div>
+        <div class="statement-container">
+          <h2>COMING SOON</h2>
+          <br />
+          <h1 style="font-size: 28px !important">
+            Sign up for news and updates on the upcoming HALO VR-6
+          </h1>
+
+          <div
+            class="d-md-flex justify-content-start form-card col-12 col-md-4"
+          >
+            <input
+              type="text"
+              class="col-12 col-md-8 input-rounded"
+              placeholder="Email"
+            />
+            <button class="col-12 col-md-4 signup-button">Sign-up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!-- FOOTER -->
+  <footer>
+    <div class="row">
+      <div class="col-12 aligncenter">
+        <ul>
+          <li class="goTop">
+            <i class="fa fa-arrow-up fa-3x" aria-hidden="true"></i>
+          </li>
+        </ul>
+
+        <h6>© 2022 - Jason S Fernandes</h6>
+      </div>
+    </div>
+  </footer>
+
+  <!-- FOR SCROLLING TO SECTIONS -->
+  <script>
+
+    let sectionHero = $('#sectionHero').offset().top,
+        sectionAbout = $('#sectionAbout').offset().top,
+        sectionProducts = $('#sectionProducts').offset().top,
+        sectionUpcoming = $('#sectionUpcoming').offset().top;
+
+    const scrollTo = (position, offset = 0) => {
+      let speed = 800;
+
+      $('html, body').animate(
+        { scrollTop: position - offset },
+        speed
+      );
+    }
+
+    $('.goAbout').click(function () {
+      scrollTo(sectionAbout, 80);
+    });
+    $('.goProducts').click(function () {
+      scrollTo(sectionProducts, 130);
+    });
+    $('.goUpcoming').click(function () {
+      scrollTo(sectionUpcoming, 130);
+    });
+    $('.goTop').click(function () { 
+      scrollTo(sectionHero, 80);
+    });
+
+  </script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/avix', function () {
     return view('coding.avix.avixHome');
 });
-Route::get('/avix/about', function () {
-    return view('coding.avix.avixAbout');
-});
-Route::get('/avix/products', function () {
-    return view('coding.avix.avixProducts');
-});
-Route::get('/avix/contact', function () {
-    return view('coding.avix.avixContact');
-});
 Route::get('/calculator', function () {
     return view('coding.calculator.calculatorApp');
 });
-// Route::get('/weather', function () {
-//     return view('coding.weather.weatherApp');
-// });
 
 // Main SPA
 Route::get('/{any}', function () {
