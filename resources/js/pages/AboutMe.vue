dc192c41ef3075ac03a0dc830c5cbfb581057f16
Jason <jsfernandes83@gmail.com>
2023-05-19 16:55:22 -0600

    About me width adjustment

--- resources/js/pages/AboutMe.vue
+++ resources/js/pages/AboutMe.vue
@@ -536,7 +536,7 @@ export default {
 
 @media (min-width: 960px) {
   .about-pane {
-    width: 500px;
+    width: 460px;
     border-top-left-radius: 10px !important;
     border-bottom-left-radius: 10px !important;
   }
